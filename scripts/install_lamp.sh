d41959df84e632c7cd67e594b5de21bbe884fafd
pablo <pablo@email.com>
2025-05-04 19:46:44 +0000

    cambios

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -14,7 +14,23 @@ zypper -n install mysql-server
 # Iniciamos mysqld
 systemctl start mysqld
 
-# Instalación del interprete de php
-zypper -n install php
+# Instalación de php, debo poner php8 porque no detecta la original
+zypper -n install php8
 
+# Se puede comprobar la versión con
+# php -v
 
+# Instalación de la extensión de PHP para que se conecte con el mysql-server
+zypper -n install php8-mysql
+
+# Reiniciamos apache2 después de la instalación
+systemctl restart apache2
+
+# Esto me ayudó chatgpt: ver versiones de php
+# zypper se -s php | grep ^i
+
+# Instalación de PHPMyAdmin
+sudo zypper -n install php-mbstring php-zip php-json php-gd php-fpm php-xml
+
+# Reincio 
+systemctl restart apache2
