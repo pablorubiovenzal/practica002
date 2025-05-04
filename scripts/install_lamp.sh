6b75e251e06116a155d823acef09adb877315b87
pablo <pablo@email.com>
2025-05-04 17:01:55 +0000

    pequeños cambio

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -8,3 +8,13 @@ zypper update
 # Instalación del servidor web apache
 zypper -n install httpd
 
+# Instalación del paquete de MySQL
+zypper -n install mysql-server
+
+# Iniciamos mysqld
+systemctl start mysqld
+
+# Instalación del interprete de php
+zypper -n install php
+
+
